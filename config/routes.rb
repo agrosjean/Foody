3dfdd35b88a39c03d85c9bcbe310ed4e9fb3b2fe
alexandre <agrosjean007@gmail.com>
2022-01-27 22:03:31 -0500

    match w/ react app routes first before api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   resources :reviews
   resources :restaurants
   post "/login",  to: "users#login"
   delete "/logout", to: "users#logout"
   resources :users
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  
 end
